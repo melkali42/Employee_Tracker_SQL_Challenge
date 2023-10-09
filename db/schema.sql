bd0000686901567b3a8772a03683230b8a8efff7
Melissa <melissakalish2@gmail.com>
2023-10-09 18:42:02 -0500

    fixed table errors for mysql suntax errors

--- db/schema.sql
+++ db/schema.sql
@@ -4,24 +4,28 @@ CREATE DATABASE employee_tracker_db;
 USE employee_tracker_db;
 
 CREATE TABLE department (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30) NOT NULL,
-  salary DECIMAL NOT NULL,
-  department_id INTEGER NULL,
-  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NULL,
+    salary DECIMAL NOT NULL, 
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id) on delete cascade,
+    PRIMARY KEY (id)
 );
 
+
 CREATE TABLE employee (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INTEGER NULL,
-  manager_id INTEGER NULL,
-  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id),
-  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL, 
+    FOREIGN KEY (role_id) REFERENCES role(id) on delete cascade,
+    manager_id INT NOT NULL,
+    FOREIGN KEY (manager_id) REFERENCES role(id) on delete set null,
+    PRIMARY KEY (id)
 );
